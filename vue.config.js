1a8c59b0207046115b239924713d73aeb8374d62
JackPeng <1105521265@qq.com>
2020-07-16 09:20:17 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 let path = require('path')
 
 module.exports = {
-    indexPath: path.resolve(__dirname, './docs/index.html'), // 指定生成的 index.html 的输出路径 (相对于 outputDir)。也可以是一个绝对路径。
-    outputDir: path.resolve(__dirname, './docs')
+    publicPath: process.env.NODE_ENV === 'production' ? '/docs/' : '/',
+    outputDir: path.resolve(__dirname, './docs'),
+    // assetsDir: path.resolve(__dirname, '.docs/'),// 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
+    indexPath: path.resolve(__dirname, './docs/index.html') // 指定生成的 index.html 的输出路径 (相对于 outputDir)。也可以是一个绝对路径。
 }
