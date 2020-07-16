ceb6cca5db0b8f7afdf6a1298fdde5a77ec1f1b9
Jack <1105521265@qq.com>
2020-07-16 18:10:36 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,6 @@ module.exports = {
     outputDir: path.resolve(__dirname, './docs'),
     // assetsDir: path.resolve(__dirname, '.docs/'),// 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
     indexPath: path.resolve(__dirname, './docs/index.html'), // 指定生成的 index.html 的输出路径 (相对于 outputDir)。也可以是一个绝对路径。
-    productionSourceMap: false,
-    filenameHashing: false
+    productionSourceMap: false, // 不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
+    // filenameHashing: false
 }
