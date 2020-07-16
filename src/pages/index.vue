831d35fa8c31b746e89d29ffa843850d9e71ba4e
Jack <1105521265@qq.com>
2020-07-16 17:34:34 +0800

    update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    123123
+    PERSONAL RESUME
   </div>
 </template>
 <script>
--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,7 @@ module.exports = {
     publicPath: './',
     outputDir: path.resolve(__dirname, './docs'),
     // assetsDir: path.resolve(__dirname, '.docs/'),// 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
-    indexPath: path.resolve(__dirname, './docs/index.html') // 指定生成的 index.html 的输出路径 (相对于 outputDir)。也可以是一个绝对路径。
+    indexPath: path.resolve(__dirname, './docs/index.html'), // 指定生成的 index.html 的输出路径 (相对于 outputDir)。也可以是一个绝对路径。
+    productionSourceMap: false,
+    filenameHashing: false
 }
