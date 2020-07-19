cc7b12753e440fe8a36bcc4fb5b1b0ee78ee7ba1
JackPeng <1105521265@qq.com>
2020-07-19 23:06:32 +0800

    update

--- src/pages/content/index.vue
+++ src/pages/content/index.vue
@@ -1,135 +1,45 @@
 <template>
   <div class="content">
     <template v-for="i of contentList">
-      <div class="resume-content"
-           :key="i.id">
+      <div class="resume-content" :key="i.id">
         <div class="resume-content__title">{{i.title}}</div>
         <div class="resume-content__subtitle">{{i.subtitle}}</div>
         <template v-if="i.textList && i.textList.length">
-          <list-render :data="i.textList"
-                       :render="render" />
+          <list-render :data="i.textList" :render="render" />
         </template>
-        <div class="resume-content__text"
-             v-else>{{i.text}}</div>
+        <div class="resume-content__text" v-else>{{i.text}}</div>
       </div>
     </template>
   </div>
 </template>
 <script>
-  import listRender from './list-render.vue'
-  export default {
-    name: 'resume-content',
-    components: { listRender },
-    data () {
-      return {
-        contentList: [
-          {
-            id: 1,
-            title: 'HELLO WORLD',
-            subtitle: 'This is subtitle',
-            text:
-              'To be a worthy person. To be a worthy person. To be a worthy person. To be a worthy person. To be a worthy person. '
-          },
-          {
-            id: 2,
-            title: '你好 世界',
-            subtitle: '这是一个副标题',
-            textList: [
-              {
-                id: 1,
-                list: 'ol',
-                text: ['写一段这里都是对应的内容。写一段这里都是对应的内容。写一段这里都是对应的内容。写一段这里都是对应的内容。', '写一段这里都是对应的内容。写一段这里都是对应的内容。写一段这里都是对应的内容。写一段这里都是对应的内容。']
-              },
-              {
-                id: 2,
-                list: 'ul',
-                text: ['写一段这里都是对应的内容。写一段这里都是对应的内容。写一段这里都是对应的内容。写一段这里都是对应的内容。']
-              }
-            ]
-          },
-          {
-            id: 3,
-            title: '你好 世界',
-            subtitle: '这是一个副标题',
-            textList: [
-              {
-                id: 1,
-                list: 'ol',
-                text: ['写一段这里都是对应的内容。写一段这里都是对应的内容。写一段这里都是对应的内容。写一段这里都是对应的内容。']
-              },
-              {
-                id: 2,
-                list: 'ul',
-                text: ['写一段这里都是对应的内容。写一段这里都是对应的内容。写一段这里都是对应的内容。写一段这里都是对应的内容。']
-              }
-            ]
-          },
-          {
-            id: 3,
-            title: '你好 世界',
-            subtitle: '这是一个副标题',
-            textList: [
-              {
-                id: 1,
-                list: 'ol',
-                text: ['写一段这里都是对应的内容。写一段这里都是对应的内容。写一段这里都是对应的内容。写一段这里都是对应的内容。']
-              },
-              {
-                id: 2,
-                list: 'ul',
-                text: ['写一段这里都是对应的内容。写一段这里都是对应的内容。写一段这里都是对应的内容。写一段这里都是对应的内容。']
-              }
-            ]
-          },
-          {
-            id: 3,
-            title: '你好 世界',
-            subtitle: '这是一个副标题',
-            textList: [
-              {
-                id: 1,
-                list: 'ol',
-                text: ['写一段这里都是对应的内容。写一段这里都是对应的内容。写一段这里都是对应的内容。写一段这里都是对应的内容。']
-              },
-              {
-                id: 2,
-                list: 'ul',
-                text: ['写一段这里都是对应的内容。写一段这里都是对应的内容。写一段这里都是对应的内容。写一段这里都是对应的内容。']
-              }
-            ]
-          },
-          {
-            id: 3,
-            title: '你好 世界',
-            subtitle: '这是一个副标题',
-            textList: [
-              {
-                id: 1,
-                list: 'ol',
-                text: ['写一段这里都是对应的内容。写一段这里都是对应的内容。写一段这里都是对应的内容。写一段这里都是对应的内容。']
-              },
-              {
-                id: 2,
-                list: 'ul',
-                text: ['写一段这里都是对应的内容。写一段这里都是对应的内容。写一段这里都是对应的内容。写一段这里都是对应的内容。']
-              }
-            ]
-          },
-        ]
-      }
-    },
-    methods: {
-      render (h, data) {
-        let tag = data.list ? data.list : 'div'
-        return <tag>{
-          data.text.map(el => {
+import listRender from './list-render.vue'
+import datas from '../data.json'
+export default {
+  name: 'resume-content',
+  components: { listRender },
+  data() {
+    return {
+      contentList: []
+    }
+  },
+  mounted() {
+    this.contentList = datas
+  },
+  methods: {
+    render(h, data) {
+      let tag = data.list ? data.list : 'div'
+      return (
+        <tag>
+          {data.text.map(el => {
             return <li style={{ marginBottom: '10px' }}>{el}</li>
-          })
-        }</tag>
-      }
-    },
+          })}
+        </tag>
+      )
+    }
   }
+}
 </script>
 <style lang="scss" scoped>
-  @import "./index.scss";
+@import './index.scss';
 </style>
