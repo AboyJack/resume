fa5151226e6869baf5eccb7bf62697f8cde90d27
JackPeng <1105521265@qq.com>
2020-07-21 09:46:56 +0800

    update

--- src/pages/content/index.vue
+++ src/pages/content/index.vue
@@ -4,10 +4,15 @@
       <div class="resume-content" :key="i.id">
         <div class="resume-content__title" v-if="i.title">{{i.title}}</div>
         <div class="resume-content__subtitle" v-if="i.subtitle">{{i.subtitle}}</div>
+        <template v-if="i.text && typeof i.text === 'string'">
+          <div class="resume-content__text">{{i.text}}</div>
+        </template>
         <template v-if="i.textList && i.textList.length">
           <list-render :data="i.textList" :render="render" />
         </template>
-        <div class="resume-content__text" v-else>{{i.text}}</div>
+        <template v-if="i.text && typeof i.text !== 'string' && i.text.length">
+          <div class="resume-content__text" v-for="(text, index) in i.text" :key="index">{{text}}</div>
+        </template>
       </div>
     </template>
   </div>
@@ -25,7 +30,7 @@ export default {
     }
   },
   mounted() {
-    const demo = true
+    const demo = false
     this.contentList = demo ? datas : resumeData
   },
   methods: {
