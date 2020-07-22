848dcf271a273236ad861886288e9c3cc61fda40
Jack <1105521265@qq.com>
2020-07-22 14:12:08 +0800

    update

--- src/components/toast/index.vue
+++ src/components/toast/index.vue
@@ -1,138 +1,54 @@
 <template>
-  <div class="toast" v-show="isShow" ref="toast" id="toast">{{message}}</div>
+  <div>
+    <transition name="fade">
+      <div v-if="isShow"
+           class="toast">{{message}}</div>
+    </transition>
+  </div>
 </template>
 <script>
-export default {
-  name: 'toast',
-  data() {
-    return {
-      isShow: false,
-      message: ''
-    }
-  },
-  mounted() {
-    this.$refs.toast.addEventListener('animationend', () => {
-      this.message = ''
-      this.isShow = false
-    })
-  },
-  methods: {
-    show(message, duration = 2000) {
-      this.message = message
-      this.isShow = true
-      this.$refs.toast.classList.add('fadeIn')
-      setTimeout(() => {
-        this.$refs.toast.classList.remove('fadeIn')
-        this.$refs.toast.classList.remove('fadeOut')
-      }, duration)
+  export default {
+    name: 'toast',
+    data () {
+      return {
+        isShow: false,
+        message: ''
+      }
+    },
+    methods: {
+      show (message, duration = 2000) {
+        this.message = message
+        this.isShow = true
+        setTimeout(() => {
+          this.isShow = false
+          this.message = ''
+        }, duration)
+      }
     }
   }
-}
 </script>
 <style lang="scss" scoped>
-@keyframes fadeIn {
-  0% {
+  .toast {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    color: #9090aa;
+    padding: 8px;
+    font-size: 14px;
+    border-radius: 4px;
+    text-align: center;
+    transform: translate(-50%, -50%);
+    -webkit-transform: translate(-50%, -50%);
+    -moz-transform: translate(-50%, -50%);
+    -o-transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    background-color: rgba(243, 246, 254, 0.8);
+  }
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.5s;
+  }
+  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
     opacity: 0;
   }
-  100% {
-    opacity: 1;
-  }
-}
-@-webkit-keyframes fadeIn {
-  0% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 1;
-  }
-}
-@-moz-keyframes fadeIn {
-  0% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 1;
-  }
-}
-@-o-keyframes fadeIn {
-  0% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 1;
-  }
-}
-@-ms-keyframes fadeIn {
-  0% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 1;
-  }
-}
-@keyframes fadeOut {
-  0% {
-    opacity: 1;
-  }
-  100% {
-    opacity: 0;
-  }
-}
-@-webkit-keyframes fadeOut {
-  0% {
-    opacity: 1;
-  }
-  100% {
-    opacity: 0;
-  }
-}
-@-moz-keyframes fadeOut {
-  0% {
-    opacity: 1;
-  }
-  100% {
-    opacity: 0;
-  }
-}
-@-o-keyframes fadeOut {
-  0% {
-    opacity: 1;
-  }
-  100% {
-    opacity: 0;
-  }
-}
-@-ms-keyframes fadeOut {
-  0% {
-    opacity: 1;
-  }
-  100% {
-    opacity: 0;
-  }
-}
-.toast {
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  color: #9090aa;
-  padding: 8px;
-  font-size: 14px;
-  border-radius: 4px;
-  text-align: center;
-  transform: translate(-50%, -50%);
-  -webkit-transform: translate(-50%, -50%);
-  -moz-transform: translate(-50%, -50%);
-  -o-transform: translate(-50%, -50%);
-  -ms-transform: translate(-50%, -50%);
-  background-color: rgba(243, 246, 254, 0.8);
-}
-.hide {
-  display: none;
-}
-.fadeOut {
-  animation: fadeOut 1s;
-}
-.fadeIn {
-  animation: fadeIn 1s;
-}
 </style>
--- src/pages/top-header/index.vue
+++ src/pages/top-header/index.vue
@@ -8,12 +8,13 @@
       </a>
     </div>
     <div class="download-list">
-      <a @click="copyEmail">
+      <a @click="copyText(email, '邮箱')">
         <i class="iconfont iconemail"></i>
         {{email}}
       </a>
     </div>
-    <div class="download-list" ref="downloadList">
+    <div class="download-list"
+         ref="downloadList">
       <a @click="downloadPDF">
         <i class="iconfont iconpdf"></i>PDF
       </a>
@@ -27,160 +28,158 @@
   </div>
 </template>
 <script>
-// import pdfFile from '@/assets/file/resume.pdf'
-// import pdf from 'vue-pdf'
-// import pdf from '@/components/pdf';
-// import download from '@/utils/download'
-import axios from 'axios'
-export default {
-  name: 'top-header',
-  components: {
-    // pdf
-  },
-  // created () {
-  //   this.pdfSrc = pdf.createLoadingTask(this.pdfSrc)
-  //   console.log(this.pdfSrc)
-  // },
-  data() {
-    return {
-      title: 'PERSONAL RESUME',
-      phone: '18128845330',
-      email: '1105521265@qq.com',
-      // pdfSrc: 'https://github.com/AboyJack/resume/raw/master/src/assets/file/resume.pdf',
-      pdfSrc:
-        'https://raw.githubusercontent.com/AboyJack/resume/master/src/assets/file/resume.pdf',
-      htmlTitle: 'PERSONAL RESUME',
-      pdfElement: '#app'
-      // pdfSrc: 'https://raw.githubusercontent.com/AboyJack/resume/master/src/assets/file/resume.pdf'
-    }
-  },
-  methods: {
-    copyEmail() {
-      this.$copyText(this.email)
-        .then(() => {
-          // console.log(res)
-          this.$toast.show('邮箱已复制到粘贴板', 2000)
-        })
-        .catch(() => {
-          // console.log(err + '复制失败')
-        })
+  // import pdfFile from '@/assets/file/resume.pdf'
+  // import pdf from 'vue-pdf'
+  // import pdf from '@/components/pdf';
+  // import download from '@/utils/download'
+  import axios from 'axios'
+  export default {
+    name: 'top-header',
+    components: {
+      // pdf
     },
-    downloadPDF() {
-      // download(this.pdfSrc, 'PERSONAL RESUME.pdf')
-      // this.exportSavePdf()
-      axios
-        .post(this.pdfSrc, {
-          responseType: 'blob'
-        })
-        .then(res => {
-          let ele = document.createElement('a')
-          ele.download = 'PERSONAL RESUME.pdf' // 下载的名称
-          // ele.target = target // 规定在何处打开链接文档
-          ele.style.display = 'none'
-          // 字符内容转变成blob地址 Blob用法 https://developer.mozilla.org/zh-CN/docs/Web/API/Blob
-          let blob = new Blob([res.data])
-          ele.href = URL.createObjectURL(blob)
-          // console.log(blob, ele)
-          document.body.appendChild(ele)
-          ele.click()
-          document.body.removeChild(ele)
-        })
-    }
-  }
-}
-</script>
-<style lang="scss" scoped>
-@import '~@/assets/style/index.scss';
-.resume-header {
-  // position: absolute;
-  // right: 0;
-  // left: 0;
-  position: relative;
-  -webkit-flex-shrink: 0;
-  flex-shrink: 0;
-  background-color: $-primary-7;
-  height: px-rem(100);
-  line-height: px-rem(110);
-  @include border-bottom-split;
-  display: flex;
-  h1 {
-    font-size: px-rem(30);
-    padding-left: px-rem(50);
-    color: $-font-title;
-    width: px-rem(330);
-    @include custom-font-family($f-m-s-s);
-  }
-  .download-list {
-    color: $-font-title;
-    font-size: 12px;
-    padding-left: px-rem(50);
-    /* flex: 1; */
-    align-self: center;
-    a {
-      color: $-font-title;
-      &:hover {
-        color: $-font-color;
-        cursor: pointer;
+    // created () {
+    //   this.pdfSrc = pdf.createLoadingTask(this.pdfSrc)
+    //   console.log(this.pdfSrc)
+    // },
+    data () {
+      return {
+        title: 'PERSONAL RESUME',
+        phone: '18128845330',
+        email: '1105521265@qq.com',
+        pdfSrc:
+          'https://raw.githubusercontent.com/AboyJack/resume/master/src/assets/file/resume.pdf',
+        htmlTitle: 'PERSONAL RESUME',
+        pdfElement: '#app'
+      }
+    },
+    methods: {
+      copyText (val, title = '内容') {
+        this.$copyText(val)
+          .then(() => {
+            // console.log(res)
+            this.$toast.show(`${title}已复制到粘贴板`)
+          })
+          .catch(() => {
+            // console.log(err + '复制失败')
+          })
+      },
+      downloadPDF () {
+        // download(this.pdfSrc, 'PERSONAL RESUME.pdf')
+        // this.exportSavePdf()
+        axios
+          .post(this.pdfSrc, {
+            responseType: 'blob'
+          })
+          .then(res => {
+            let ele = document.createElement('a')
+            ele.download = 'PERSONAL RESUME.pdf' // 下载的名称
+            // ele.target = target // 规定在何处打开链接文档
+            ele.style.display = 'none'
+            // 字符内容转变成blob地址 Blob用法 https://developer.mozilla.org/zh-CN/docs/Web/API/Blob
+            let blob = new Blob([res.data])
+            ele.href = URL.createObjectURL(blob)
+            // console.log(blob, ele)
+            document.body.appendChild(ele)
+            ele.click()
+            document.body.removeChild(ele)
+          })
       }
     }
-    i {
-      position: relative;
-      right: 3px;
-      /* top: 3px; */
-    }
-    &:before {
-      content: '';
-      position: relative;
-      right: 10px;
-      width: 10px;
-      border-left: 1px solid $-split-color;
-    }
   }
-  .more-list {
-    text-align: right;
-    padding-right: px-rem(50);
-    flex: 1;
-    &:before {
-      border-left-width: 0px;
+</script>
+<style lang="scss" scoped>
+  @import "~@/assets/style/index.scss";
+  .resume-header {
+    // position: absolute;
+    // right: 0;
+    // left: 0;
+    position: relative;
+    -webkit-flex-shrink: 0;
+    flex-shrink: 0;
+    background-color: $-primary-7;
+    height: px-rem(100);
+    line-height: px-rem(110);
+    @include border-bottom-split;
+    display: flex;
+    h1 {
+      font-size: px-rem(30);
+      padding-left: px-rem(50);
+      color: $-font-title;
+      width: px-rem(330);
+      @include custom-font-family($f-m-s-s);
     }
-    i {
-      font-size: px-rem(50);
-    }
-  }
-  @mixin show-download-list {
     .download-list {
-      display: block;
+      color: $-font-title;
+      font-size: 12px;
+      padding-left: px-rem(50);
+      /* flex: 1; */
+      align-self: center;
+      a {
+        color: $-font-title;
+        &:hover {
+          color: $-font-color;
+          cursor: pointer;
+        }
+      }
+      i {
+        position: relative;
+        right: 3px;
+        top: 1px;
+      }
+      &:before {
+        content: "";
+        position: relative;
+        right: 12px;
+        width: 10px;
+        border-left: 1px solid $-split-color;
+      }
     }
     .more-list {
-      display: none;
-    }
-  }
-  @mixin show-more {
-    .download-list {
-      display: none;
-    }
-    .more-list {
-      display: block;
-    }
-  }
-  @media screen and ($media-max: $w-500) {
-    .download-list {
       text-align: right;
       padding-right: px-rem(50);
+      flex: 1;
       &:before {
         border-left-width: 0px;
       }
+      i {
+        font-size: px-rem(45);
+      }
     }
-    @include show-more;
-  }
-  @media screen and ($media-min: $w-500) and ($media-max: $w-768) {
-    @include show-download-list;
-  }
-  @media screen and ($media-min: $w-1000) {
-    h1 {
-      padding-left: 40px;
+    @mixin show-download-list {
+      .download-list {
+        display: block;
+      }
+      .more-list {
+        display: none;
+      }
     }
-    @include show-download-list;
+    @mixin show-more {
+      .download-list {
+        display: none;
+      }
+      .more-list {
+        display: block;
+      }
+    }
+    @media screen and ($media-max: $w-500) {
+      .download-list {
+        text-align: right;
+        padding-right: px-rem(50);
+        &:before {
+          border-left-width: 0px;
+        }
+      }
+      @include show-more;
+    }
+    @media screen and ($media-min: $w-500) and ($media-max: $w-768) {
+      @include show-download-list;
+    }
+    @media screen and ($media-min: $w-1000) {
+      h1 {
+        padding-left: 40px;
+      }
+      @include show-download-list;
+    }
   }
-}
 </style>
